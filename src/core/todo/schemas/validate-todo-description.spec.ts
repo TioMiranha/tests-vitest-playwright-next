989e24174c6ccd9d870840eaa6e7863c63c08447
Eduardo Silva <eduardosilvajs.py@gmail.com>
2025-08-04 16:43:30 -0400

    Apenas corrigin um pequeno erro durante os testes

--- src/core/todo/schemas/validate-todo-description.spec.ts
+++ src/core/todo/schemas/validate-todo-description.spec.ts
@@ -1,11 +1,13 @@
 import { validateTodoDescription } from "./validate-todo-description";
 
 describe('validateTodoDescription (unit)', () => {
+
   test('deve retornar erros quando a descrição tem menos que 4 caracteres', () => {
     const description = 'abc';
     const result = validateTodoDescription(description);
+
     expect(result.errors).toStrictEqual([
-      'Descrição deve ter mais do que 3 caracteres',
+      'Descrição precisa ter mais de 3 caracteres',
     ]);
     expect(result.sucsses).toBe(false);
   });
@@ -13,6 +15,7 @@ describe('validateTodoDescription (unit)', () => {
   test('deve retornar sucesso quando a descrição tem mais de 3 caracteres', () => {
     const description = 'abcd';
     const result = validateTodoDescription(description);
+
     expect(result.errors).toStrictEqual([]);
     expect(result.sucsses).toBe(true);
   });
