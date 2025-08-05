9787ee4c06526b62c6257ea4b676cc0d0f86f606
Eduardo Silva <eduardosilvajs.py@gmail.com>
2025-08-05 10:40:10 -0400

    Adicionando variáveis de ambiente para realizar testes

--- src/utils/sanitize-str.ts
+++ src/utils/sanitize-str.ts
@@ -1,3 +1,8 @@
+import { getFullEnv } from "@/env/config";
+
 export function sanitizeStr(s: string): string {
   return !s || typeof s != 'string' ? '' : s.trim().normalize();
 }
+
+const env = getFullEnv();
+console.log(env);
