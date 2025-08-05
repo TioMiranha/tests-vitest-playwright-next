8b3d583b696548d17691d00b93dafc1c6d861420
Eduardo Silva <eduardosilvajs.py@gmail.com>
2025-08-05 13:55:00 -0400

    Adicionando migrates com drizzle

--- src/utils/sanitize-str.ts
+++ src/utils/sanitize-str.ts
@@ -1,8 +1,3 @@
-import { getFullEnv } from "@/env/config";
-
 export function sanitizeStr(s: string): string {
   return !s || typeof s != 'string' ? '' : s.trim().normalize();
 }
-
-const env = getFullEnv();
-console.log(env);
