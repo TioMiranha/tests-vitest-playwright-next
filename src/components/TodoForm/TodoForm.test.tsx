02ac5c14ec6a65033dd169899b481c9415bee611
Eduardo Silva <eduardosilvajs.py@gmail.com>
2025-08-11 15:54:09 -0400

    Coloquei tanta coisa que nem adianta dizer específicamente o que eu fiz, só sei que ao commitar as modificações mais recentes foram as de adicionar o playwrigth

--- src/components/TodoForm/TodoForm.test.tsx
+++ src/components/TodoForm/TodoForm.test.tsx
@@ -1,7 +1,6 @@
 import userEvent from "@testing-library/user-event";
-import { todo } from "node:test";
 import { TodoForm } from ".";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 
 const user = userEvent.setup();
 
@@ -14,21 +13,80 @@ describe('<TodoForm />', () => {
     expect(input).toBeInTheDocument();
   });
 
-  test('deve chamar a action com os valores corretos', async () => { });
+  test('deve chamar a action com os valores corretos', async () => {
+    const { btn, input, action } = renderForm();
 
-  test('deve cortar espaços do incio e fim da description (trim)', async () => { });
+    await user.type(input, 'tarefa');
+    await user.click(btn);
 
-  test('deve limpar o input se o formulário retornar sucesso', async () => { });
+    expect(action).toHaveBeenCalledExactlyOnceWith('tarefa');
+  });
 
-  test('deve desativar o botão enquanto envia a action', async () => { });
+  test('deve cortar espaços do incio e fim da description (trim)', async () => {
+    const { btn, input, action } = renderForm();
 
-  test('deve desativar o input enquanto envia a action', async () => { });
+    await user.type(input, '   tarefa   ');
+    await user.click(btn);
 
-  test('deve trocar o texto do botão enquanto envia a action', async () => { });
+    expect(action).toHaveBeenCalledExactlyOnceWith('tarefa');
+  });
 
-  test('deve mostrar o erro quando a action retornar erro', async () => { });
+  test('deve limpar o input se o formulário retornar sucesso', async () => {
+    const { btn, input } = renderForm();
 
-  test('deve manter o texto digitado no input se a action retornar erro', async () => { });
+    await user.type(input, '   tarefa   ');
+    await user.click(btn);
+
+    expect(input).toHaveValue('');
+  });
+
+  test('deve desativar o botão enquanto envia a action', async () => {
+    const { btn, input } = renderForm({ delay: 5 });
+
+    await user.type(input, 'tarefa');
+    await user.click(btn);
+    await waitFor(() => expect(btn).toBeDisabled());
+    await waitFor(() => expect(btn).toBeEnabled());
+  });
+
+  test('deve desativar o input enquanto envia a action', async () => {
+    const { btn, input } = renderForm({ delay: 5 });
+
+    await user.type(input, 'tarefa');
+    await user.click(btn);
+    await waitFor(() => expect(input).toBeDisabled());
+    await waitFor(() => expect(input).toBeEnabled());
+  });
+
+  test('deve trocar o texto do botão enquanto envia a action', async () => {
+    const { btn, input } = renderForm({ delay: 5 });
+    await user.type(input, 'tarefa');
+    await user.click(btn);
+
+    await waitFor(() => expect(btn).toHaveAccessibleName('Criando tarefa'));
+    await waitFor(() => expect(btn).toHaveAccessibleName('Criar tarefa'));
+
+  });
+
+  test('deve mostrar o erro quando a action retornar erro', async () => {
+    const { btn, input } = renderForm({ success: false });
+    await user.type(input, 'tarefa');
+    await user.click(btn);
+
+    const error = await screen.findByRole('alert');
+
+    expect(error).toHaveTextContent('falha ao criar todo');
+    expect(input).toHaveAttribute('aria-describedby', error.id);
+
+  });
+
+  test('deve manter o texto digitado no input se a action retornar erro', async () => {
+    const { btn, input } = renderForm({ success: false });
+    await user.type(input, 'tarefa');
+    await user.click(btn);
+
+    expect(input).toHaveValue('tarefa');
+  });
 
 });
 
---
+++ tests/home.e2e.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('<Home /> (E2E)', () => {
+  // Renderização
+
+  // Criação
+
+  // Exclusão
+
+  // Erros
+});
